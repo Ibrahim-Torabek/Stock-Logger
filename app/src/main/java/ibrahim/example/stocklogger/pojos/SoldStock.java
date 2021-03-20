ba65d7698f5319baa4d8d6218801d9f45c8e0045
Wusiman Yibulayin <wusiman.yibulayin01@stclairconnect.ca>
2021-03-19 22:47:20 -0400

    Store Active Stocks #2    * Used method to get UNIQUE symbols to identify how many stocks the user have    * Used method to get stocks by stock symbol.

---
+++ app/src/main/java/ibrahim/example/stocklogger/pojos/SoldStock.java
@@ -0,0 +1,22 @@
+package ibrahim.example.stocklogger.pojos;
+
+import android.os.Parcelable;
+
+/**
+ * <h1>App for Stock Logger</h1>
+ * <h2>Android project of MAD405 Course</h2>
+ *
+ * This is a class for sold stocks. It stores information of stock
+ * which user sold. We will pass the object of this class among fragments,
+ * so we need to implement Parcelable interface.
+ *
+ * @see Parcelable
+ *
+ * @author Ibrahim (Wusiman Yibuulayin)
+ * @version 1.0
+ * @since 2021-03-20
+ */
+
+
+public class SoldStock {
+}
