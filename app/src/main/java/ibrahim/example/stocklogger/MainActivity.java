e6c1346b327af71e6ab1f04abf4e8f5ca6cd005c
Wusiman Yibulayin <wusiman.yibulayin01@stclairconnect.ca>
2021-04-04 22:28:21 -0400

    SoldStockFragment Added

--- app/src/main/java/ibrahim/example/stocklogger/MainActivity.java
+++ app/src/main/java/ibrahim/example/stocklogger/MainActivity.java
@@ -48,7 +48,7 @@ public class MainActivity extends AppCompatActivity implements DatePickerDialog.
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_main)
+                R.id.nav_main, R.id.nav_sold)
                 .setDrawerLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
