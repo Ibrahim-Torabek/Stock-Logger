5bddf9bc8c709099119938e307b9d7ce7acb4ab4
Wusiman Yibulayin <wusiman.yibulayin01@stclairconnect.ca>
2021-04-10 14:11:07 -0400

    Contact Call Page Completed

--- app/src/main/java/ibrahim/example/stocklogger/fragments/contact/ContactCallFragment.java
+++ app/src/main/java/ibrahim/example/stocklogger/fragments/contact/ContactCallFragment.java
@@ -1,12 +1,21 @@
 package ibrahim.example.stocklogger.fragments.contact;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.google.android.material.snackbar.Snackbar;
 
 import ibrahim.example.stocklogger.R;
 
@@ -21,6 +30,8 @@ public class ContactCallFragment extends Fragment {
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
+
+    public static final int PERMISSION_CALL_PHONE = 0;
 
     // TODO: Rename and change types of parameters
     private String mParam1;
@@ -61,6 +72,72 @@ public class ContactCallFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_contact_call, container, false);
+        View view = inflater.inflate(R.layout.fragment_contact_call, container, false);
+
+        TextView callTextView = view.findViewById(R.id.callTextView);
+        callTextView.setOnClickListener(new PhoneCall(callTextView.getText().toString()));
+
+        return view;
+    }
+
+    /**
+     * <h1>App for Stock Logger</h1>
+     * <h2>Android Final Project of MAD305 Course</h2>
+     *
+     *  OnClickListener class implemets {@link android.view.View.OnClickListener}<br>
+     *  get call number as string in constructor method<br>
+     *  open phone app with call number in onClick method<br>
+     *
+     * @author Wusiman Yibuulayin
+     * @version 1.0
+     * @since 10/04/21
+     *
+     */
+    class PhoneCall implements View.OnClickListener{
+        private String callNumber;
+
+        /**
+         * Constructor to get the call number
+         * @param callNumber
+         */
+        public PhoneCall(String callNumber) {
+            this.callNumber = callNumber;
+            //this.view = view;
+        }
+
+        @Override
+        public void onClick(View v) {
+            if(ContextCompat.checkSelfPermission(getActivity(),
+                    Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED){
+                Snackbar snackbar = Snackbar.make(getActivity().findViewById(android.R.id.content),"Permission NOT Granted", Snackbar.LENGTH_LONG);
+                snackbar.show();
+
+                if(ActivityCompat.shouldShowRequestPermissionRationale(
+                        getActivity(),
+                        Manifest.permission.CALL_PHONE
+                )){
+
+                } else {
+                    ActivityCompat.requestPermissions(
+                            getActivity(),
+                            new String[] {Manifest.permission.CALL_PHONE},
+                            PERMISSION_CALL_PHONE
+                    );
+                }
+            } else {
+
+                Intent i = new Intent(Intent.ACTION_VIEW);
+                Uri call = Uri.parse("tel:" + callNumber);
+                i.setData(call);
+
+                if(i.resolveActivity(getActivity().getPackageManager()) != null){
+                    startActivity(i);
+                } else {
+                    Snackbar snackbar = Snackbar.make(getActivity().findViewById(android.R.id.content),"Cannot make a call", Snackbar.LENGTH_LONG);
+                    snackbar.show();
+                }
+            }
+
+        }
     }
 }
